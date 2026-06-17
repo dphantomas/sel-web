a56da5d247bc64cdcaabff3b8d5b7ca1f6ac40ec
Dario Gabriel Geier <dario.geier@gmail.com>
2026-06-17 09:44:29 -0300

    Fix: force Google OAuth to prompt for account selection

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -9,6 +9,11 @@ export const authOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || '',
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
+      authorization: {
+        params: {
+          prompt: "consent select_account",
+        },
+      },
     }),
     CredentialsProvider({
       name: 'Credenciales',
